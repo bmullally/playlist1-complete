2b369c7b5a2b00ec120a7b1529643d55b55bf91b
Brenda Mullally <bmullally@wit.ie>
2018-01-29 13:51:06 +0000

    updated server.js

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ const routes = require('./routes');
 app.use('/', routes);
 
 const listener = app.listen(process.env.PORT || 4000, function () {
-  logger.info(`gomix-template-1 started on port ${listener.address().port}`);
+  logger.info(`glitch-playlist1 started on port ${listener.address().port}`);
 });
